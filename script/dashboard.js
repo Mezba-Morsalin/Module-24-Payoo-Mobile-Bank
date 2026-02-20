7f39a2bc20089d1571f3da03663fea3a3517491d
Mezba Morsalin <mejbahmorsalin560@hotmail.com>
2026-02-20 09:08:48 +0600

    50% Done

--- script/dashboard.js
+++ script/dashboard.js
@@ -17,29 +17,107 @@ const items = document.getElementsByClassName("div")
 }
 // Cash Out 
 document.getElementById("cash-out-btn").addEventListener("click", function () {
-    const agentNumber = getValueFromInput("cash-out-tel");
+    const agentNumber = getValueFromInput("cash-out-tel")
     if (agentNumber.length !== 11) {
         alert("Invalid Number");
         return;
     }
-    const cashOutAmount = Number(getValueFromInput("cash-out-amount"));
-    const currentBalance = document.getElementById("current-balance");
-    const currentBalanceValue = Number(currentBalance.innerText);
+
+    const cashOutAmount = Number(getValueFromInput("cash-out-amount"))
+    const currentBalance = getBalance("current-balance");
     
-    if (cashOutAmount > currentBalanceValue) {
+    if (cashOutAmount > currentBalance) {
         alert("Insufficient Balance");
         return;
     }
 
     const pin = getValueFromInput("cash-out-pin");
+
+    if (pin.length !== 4) {
+        alert("Invalid Pin");
+        return;
+    }
+    const updateBalance = currentBalance - cashOutAmount;
+    setBalance(updateBalance);
+    const time = new Date().toLocaleString()
+    alert(`Cash Out Successful From Payoo Agent ${agentNumber} at ${time}`)
+})
+// Add Money
+document.getElementById("add-money-btn").addEventListener("click", function () {
+    const selectBank = getValueFromInput("add-money-bank");
+    if (selectBank === "Select Bank") {
+        alert("Please Select a Bank");
+        return;
+    }
+
+    const bankAccountNo = getValueFromInput("add-money-tel");
+    if (bankAccountNo.length !== 11) {
+        alert("Invalid Account Number");
+        return;
+    }
     
+    const addMoneyAmount = Number(getValueFromInput("add-money-amount"));
+    if (addMoneyAmount <= 0) {
+        alert("Enter a Valid Amount");
+        return;
+    }
+    const currentBalance = getBalance("current-balance")
+
+    const pin = getValueFromInput("add-money-pin");
     if (pin.length !== 4) {
         alert("Invalid Pin");
         return;
     }
 
-    const updateBalance = currentBalanceValue - cashOutAmount;
-    currentBalance.innerText = updateBalance;
+    const updateBalance = currentBalance + addMoneyAmount;
+    setBalance(updateBalance);
+    const time = new Date().toLocaleString();
 
-    alert("Cash Out Successful")
+    alert(`Your Money Added Successfully From ${selectBank} at ${time}`)
+})
+// Transfer Money
+document.getElementById("transfer-money-btn").addEventListener("click", function () {
+    const userNumber = getValueFromInput("transfer-money-tel");
+    if (userNumber.length !== 11) {
+        alert("Invalid Number");
+        return;
+    }
+
+    const transferAmount = Number(getValueFromInput("transfer-money-amount"));
+    const currentBalance = getBalance("current-balance");
+    if (transferAmount <= 0) {
+        alert("Enter a Valid Amount");
+        return;
+    }
+    if (transferAmount > currentBalance) {
+        alert("Insufficient Balance");
+        return;
+    }
+
+    const pin = getValueFromInput("transfer-money-pin");
+    if (pin.length !== 4) {
+        alert("Invalid Pin");
+        return;
+    }
+
+    const updateBalance = currentBalance - transferAmount;
+    setBalance(updateBalance);
+
+    const time = new Date().toLocaleString()
+
+    alert(`Money Transfer Successfully to ${userNumber} at ${time}`)
+})
+// Get bonus
+document.getElementById("get-bonus-btn").addEventListener("click", function () {
+    const bonusCode = getValueFromInput("get-bonus-amount");
+    const time = new Date().toLocaleString();
+    if (bonusCode === "mejbah123") {
+        const  currentBalance = getBalance("current-balance")
+        const updateBalance = currentBalance + 500;
+        setBalance(updateBalance)
+        alert(`Congratulations You Have Won 500$ ${time}`)
+    }
+    else {
+        alert("Invalid Coupon")
+    }
 })
--- script/machine.js
+++ script/machine.js
@@ -1,5 +1,33 @@
 function getValueFromInput (id) {
-    const input = document.getElementById(id);
-    const value = input.value;
-    return value;
+    const input = document.getElementById(id)
+    const inputValue = input.value;
+    return inputValue;
+}
+function getBalance () {
+    const getBalance = document.getElementById("current-balance");
+    const currentBalance = getBalance.innerText;
+    return Number(currentBalance);
+}
+
+function setBalance (value) {
+    const setBalance = document.getElementById("current-balance");
+    setBalance.innerText = value
+}
+
+function showOnly (show) {
+    const addMoney = document.getElementById("add-money");
+    const cashOut = document.getElementById("cash-out");
+    const transferMoney = document.getElementById("transfer-money");
+    const payment = document.getElementById("payment");
+    const getBonus = document.getElementById("get-bonus");
+
+    addMoney.classList.add("hidden");
+    cashOut.classList.add("hidden");
+    transferMoney.classList.add("hidden");
+    payment.classList.add("hidden");
+    getBonus.classList.add("hidden");
+
+    const showing = document.getElementById(show);
+    
+    showing.classList.remove("hidden");
 }
